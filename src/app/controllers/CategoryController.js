511035a4fc58ec68061016a59f55f502a81a6ee9
JozywanRamos22 <jozywan1995@gmail.com>
2023-11-10 20:21:39 -0300

    back-end

--- src/app/controllers/CategoryController.js
+++ src/app/controllers/CategoryController.js
@@ -18,6 +18,8 @@ class CategoryController {
     }
     const { name } = request.body
 
+    const { filename: path } = request.file
+
     const categoryExists = await Category.findOne({
       where: {
         name,
@@ -26,7 +28,7 @@ class CategoryController {
     if (categoryExists) {
       return response.status(401).json({ error: 'Category already exists' })
     }
-    const { id } = await Category.create({ name })
+    const { id } = await Category.create({ name, path })
 
     return response.json({ id, name })
   }
@@ -37,6 +39,41 @@ class CategoryController {
     const category = await Category.findAll()
 
     return response.json(category)
+  }
+  async update(request, response) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+    })
+    try {
+      await schema.validateSync(request.body, { abortEarly: false })
+    } catch (err) {
+      return response.status(400).json({ error: err.errors })
+    }
+    const { admin: isAdmin } = await User.findByPk(request.userId)
+    if (!isAdmin) {
+      return response.status(401).json()
+    }
+    const { name } = request.body
+
+    const { id } = request.params
+
+    const category = await Category.findByPk(id)
+    if (!category) {
+      return response
+        .status(401)
+        .json({ error: 'Make sure your category id is correct' })
+    }
+    let path
+    if (request.file) {
+      path = request.file.filename
+    }
+
+    await Category.update({ name, path }, { where: { id } })
+
+    return response.status(200).json()
+  }
+  catch(err) {
+    console.log(err)
   }
 }
 export default new CategoryController()
--- src/routes.js
+++ src/routes.js
@@ -17,9 +17,11 @@ routes.use(autMiddleware) // todas as rotas abaixo do meu middleware serão cham
 
 routes.post('/products', upload.single('file'), ProductController.store)
 routes.get('/products', ProductController.index)
+routes.put('/products/:id', upload.single('file'), ProductController.update)
 
-routes.post('/categories', CategoryController.store)
+routes.post('/categories', upload.single('file'), CategoryController.store)
 routes.get('/categories', CategoryController.index)
+routes.put('/categories/:id', upload.single('file'), CategoryController.update)
 
 routes.post('/orders', OrderController.store)
 routes.put('/orders/:id', OrderController.update)
